d1fc82554b3b042f7a3b3cf115aa650109748788
kdcriollo2025 <kdcriollo@espe.edu.ec>
2024-12-13 15:44:23 -0500

    res
    respuesta

---
+++ primerparcial/src/main/java/com/arquitectura/examen/primerparcial/PrimerparcialApplication.java
@@ -0,0 +1,13 @@
+package com.arquitectura.examen.primerparcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerparcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerparcialApplication.class, args);
+	}
+
+}
---
+++ primerparcial/src/test/java/com/arquitectura/examen/primerparcial/PrimerparcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arquitectura.examen.primerparcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerparcialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
